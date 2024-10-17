3b5498cfc3659c5602ad52690388c355a5efb4b8
akashnachane05 <nachaneakash@gmail.com>
2024-10-17 06:42:24 +0530

    donee

--- Backend/server.js
+++ Backend/server.js
@@ -1,32 +1,26 @@
-// backend/index.js
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 require('dotenv').config();
-const path =require('path')
+const path = require('path');
 const bodyParser = require('body-parser');
 const app = express();
-const PORT = process.env.PORT ;
+const PORT = process.env.PORT;
 const authRoutes = require('./routes/authRoutes');
 const faceAnalysis = require('./routes/faceAnalysis');
 const recommendationRoutes = require('./routes/recommendationRoutes');
-app.use(express.static(path.join(__dirname, 'build')));
 
-// Handle React routing, return all requests to React's index.html
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
 // Middleware
 app.use(cors());
 app.use(express.json()); // For parsing application/json
 app.use(bodyParser.json());
 app.use(cors({
-  origin: "https://my-app2-ubnu.onrender.com",
-  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Replace with your frontend's URL
+  origin: "https://my-app2-ubnu.onrender.com", // Your frontend URL
+  methods: ['GET', 'POST', 'PUT', 'DELETE'], 
   credentials: true,
 }));
 app.use(bodyParser.json({ limit: '10mb' }));
-app.use(express.urlencoded({ limit: '10mb', extended: true })); 
+app.use(express.urlencoded({ limit: '10mb', extended: true }));
 
 // Connect to MongoDB
 const mongoURI = process.env.MONGO_URI;
@@ -35,28 +29,25 @@ mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log("MongoDB connected"))
   .catch(err => console.error("MongoDB connection error:", err));
 
-
-
+// API routes
 app.use('/api2/auth2', authRoutes);
 app.use('/api2', faceAnalysis); 
 app.use('/api2', recommendationRoutes);
+
 // Test Route
 app.get('/', (req, res) => {
   res.send("Welcome to the backend!");
+});
+
+// Static file serving for the React app
+app.use(express.static(path.join(__dirname, 'build')));
+
+// Handle React routing, return all requests to React's index.html
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
 // Start Server
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
-// app.get('/api2/auth2/profile', async (req, res) => {
-//   try {
-//       // Logic to get the user's profile data
-//       const profileData = await getUserProfile(req.user.id); // Example function
-//       res.json(profileData);
-//   } catch (error) {
-//       console.error('Error fetching profile data:', error);
-//       res.status(500).json({ message: 'Internal Server Error' }); // Return JSON on error
-//   }
-// });
-
