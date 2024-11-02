f939f2b29b51dcf56e94d763325a3e002659f815
akashnachane05 <nachaneakash@gmail.com>
2024-11-02 11:04:37 +0530

    bb

--- Backend/controllers/authcontroller.js
+++ Backend/controllers/authcontroller.js
@@ -14,11 +14,7 @@ exports.signup = async (req, res) => {
         const trimmedEmail = email.trim();
         const existingUser = await User.findOne({ email: trimmedEmail });
         if (existingUser) return res.status(400).json({ message: 'User already exists' });
-
-        // const salt = await bcrypt.genSalt(10);
-        // const hashedPassword = await bcrypt.hash(password.trim(), salt); // Trim and hash the password
-
-        const newUser = new User({ name, email: trimmedEmail, password: password }); // Use hashed password
+        const newUser = new User({ name, email: trimmedEmail, password: password }); 
         await newUser.save();
 
         const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
